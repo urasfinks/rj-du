00517c8e6c85be9c4268fba4ff3719efd9f1f9a1
sfinks <urasfinks@yandex.ru>
2023-04-27 22:25:32 +0300

    Добавлена русская локализация

--- lib/dynamic_ui/widget/material_app_widget.dart
+++ lib/dynamic_ui/widget/material_app_widget.dart
@@ -4,6 +4,7 @@ import '../dynamic_ui_builder_context.dart';
 import '../widget/abstract_widget.dart';
 import '../../bottom_tab.dart';
 import '../../theme_provider.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class MaterialAppWidget extends AbstractWidget {
   @override
@@ -14,6 +15,15 @@ class MaterialAppWidget extends AbstractWidget {
       darkTheme: ThemeProvider.darkThemeData(),
       themeMode: ThemeMode.system,
       home: BottomTab(dynamicUIBuilderContext),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'), // English
+        Locale('ru'), // Spanish
+      ],
     );
   }
 }
