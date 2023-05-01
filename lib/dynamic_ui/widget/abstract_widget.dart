20aab9921c90afc50c1a375e49e1181285872de3
sfinks <urasfinks@yandex.ru>
2023-05-01 21:56:41 +0300

    Добавлено расширение Switch для управления рендеринга страницы в зависимости от условия состояния

--- lib/dynamic_ui/widget/abstract_widget.dart
+++ lib/dynamic_ui/widget/abstract_widget.dart
@@ -1,10 +1,13 @@
+import 'package:flutter/foundation.dart';
+
 import '../dynamic_ui.dart';
 import '../dynamic_ui_builder_context.dart';
 import '../../dynamic_invoke/dynamic_invoke.dart';
 import '../../util.dart';
 
 abstract class AbstractWidget {
-  dynamic get(Map<String, dynamic> parsedJson, DynamicUIBuilderContext dynamicUIBuilderContext);
+  dynamic get(Map<String, dynamic> parsedJson,
+      DynamicUIBuilderContext dynamicUIBuilderContext);
 
   static dynamic getValueStatic(
     Map<String, dynamic> parsedJson,
@@ -12,7 +15,9 @@ abstract class AbstractWidget {
     dynamic defaultValue,
     DynamicUIBuilderContext dynamicUIBuilderContext,
   ) {
-    dynamic selector = key == null ? parsedJson : ((parsedJson.containsKey(key)) ? parsedJson[key] : defaultValue);
+    dynamic selector = key == null
+        ? parsedJson
+        : ((parsedJson.containsKey(key)) ? parsedJson[key] : defaultValue);
     if (selector.runtimeType.toString() == "String") {
       selector = Util.template(selector, dynamicUIBuilderContext);
     }
@@ -25,7 +30,8 @@ abstract class AbstractWidget {
     dynamic defaultValue,
     DynamicUIBuilderContext dynamicUIBuilderContext,
   ) {
-    return getValueStatic(parsedJson, key, defaultValue, dynamicUIBuilderContext);
+    return getValueStatic(
+        parsedJson, key, defaultValue, dynamicUIBuilderContext);
   }
 
   dynamic render(
@@ -34,7 +40,8 @@ abstract class AbstractWidget {
     dynamic defaultValue,
     DynamicUIBuilderContext dynamicUIBuilderContext,
   ) {
-    return DynamicUI.render(parsedJson, key, defaultValue, dynamicUIBuilderContext);
+    return DynamicUI.render(
+        parsedJson, key, defaultValue, dynamicUIBuilderContext);
   }
 
   dynamic renderList(
@@ -47,7 +54,8 @@ abstract class AbstractWidget {
     }
     String keyDefaultBeforeUpdate = "${key}DefaultBeforeUpdate";
     if (parsedJson.containsKey(keyDefaultBeforeUpdate)) {
-      parsedJson[key] = updateList(parsedJson[keyDefaultBeforeUpdate] as List, dynamicUIBuilderContext);
+      parsedJson[key] = updateList(
+          parsedJson[keyDefaultBeforeUpdate] as List, dynamicUIBuilderContext);
     } else {
       List saveList = [];
       saveList.addAll(parsedJson[key]);
@@ -65,35 +73,14 @@ abstract class AbstractWidget {
     List<dynamic> result = [];
     for (var element in list) {
       Map<String, dynamic> el = element as Map<String, dynamic>;
-      if (el.containsKey("ChildrenExtension") && el["ChildrenExtension"] == "StateDataIterator") {
-        bool divider = el.containsKey("Divider");
-        //print("StateDataIterator(${el["key"]}) : ${dynamicUIBuilderContext.dynamicPage.stateData.value}");
-        String key = el["key"];
-        //Будем прихранивать неявно объявленные uuid, для последующего удаления, что бы небыло лишних перерисовок страницы
-        List earlyUuids = dynamicUIBuilderContext.dynamicPage.getProperty(key, []) as List;
-        for (String earlyUuid in earlyUuids) {
-          dynamicUIBuilderContext.dynamicPage.removeShadowUuid(earlyUuid);
-        }
-        dynamic value = dynamicUIBuilderContext.dynamicPage.stateData.value[key];
-        bool add = false;
-        if (value != null) {
-          List<dynamic> extraList = value as List<dynamic>;
-          for (Map<String, dynamic> data in extraList) {
-            Map<String, dynamic> newElement = {};
-            newElement.addAll(el["template"] ?? data["template"]); //Шаблон можно заложить в данные
-            newElement["context"] = data;
-            if (data.containsKey("uuid_data")) {
-              dynamicUIBuilderContext.dynamicPage.addShadowUuid(data["uuid_data"]);
-            }
-            add = true;
-            result.add(newElement);
-            if (divider && extraList.last != data) {
-              result.add(el["Divider"]);
-            }
-          }
-        }
-        if(!add && el.containsKey("ifDataEmpty")){
-          result.add(el["ifDataEmpty"]);
+      if (el.containsKey("ChildrenExtension")) {
+        switch (el["ChildrenExtension"]) {
+          case "StateDataIterator":
+            extensionStateDataIterator(el, dynamicUIBuilderContext, result);
+            break;
+          case "StateDataSwitch":
+            extensionStateDataSwitch(el, dynamicUIBuilderContext, result);
+            break;
         }
       } else {
         result.add(element);
@@ -102,19 +89,101 @@ abstract class AbstractWidget {
     return result;
   }
 
-  static void clickStatic(Map<String, dynamic> parsedJson, DynamicUIBuilderContext dynamicUIBuilderContext,
-      [String key = "onPressed"]) {
-    Map<String, dynamic>? settings = getValueStatic(parsedJson, key, null, dynamicUIBuilderContext);
-    if (settings != null) {
-      if (settings.containsKey('jsInvoke')) {
-        DynamicInvoke().jsInvoke(settings['jsInvoke'], settings['args'], dynamicUIBuilderContext);
-      } else if (settings.containsKey('sysInvoke')) {
-        DynamicInvoke().sysInvoke(settings['sysInvoke'], settings['args'] ?? {}, dynamicUIBuilderContext);
+  static void extensionStateDataSwitch(Map<String, dynamic> child,
+      DynamicUIBuilderContext dynamicUIBuilderContext, List<dynamic> result) {
+    String key = child["key"];
+
+    //Будем прихранивать неявно объявленные uuid, что бы небыло лишних перерисовок страницы
+    //Если это повторная отрисовка, данные, которые были на прошлом шагу могли поменятся
+    //Поэтому удалим их и потом заново добавим уже обновлённые
+    removeLastShadowUuid(key, dynamicUIBuilderContext);
+    String value =
+        dynamicUIBuilderContext.dynamicPage.stateData.value[key] ?? "default";
+    List<dynamic> children = child["children"];
+    Map<String, dynamic> map = {};
+    for (Map<String, dynamic> item in children) {
+      if (item.containsKey("case")) {
+        map[item["case"]] = item;
+      } else {
+        if (kDebugMode) {
+          print("extensionStateDataSwitch item key 'case' not exist");
+        }
+      }
+    }
+    if (map.containsKey(value)) {
+      if (map[value].containsKey("uuid_data")) {
+        dynamicUIBuilderContext.dynamicPage
+            .addShadowUuid(map[value]["uuid_data"]);
+      }
+      result.add(map[value]);
+    } else {
+      if (kDebugMode) {
+        print("extensionStateDataSwitch not found 'case' =  $value");
       }
     }
   }
 
-  void click(Map<String, dynamic> parsedJson, DynamicUIBuilderContext dynamicUIBuilderContext, [String key = "onPressed"]) {
+  static void extensionStateDataIterator(Map<String, dynamic> child,
+      DynamicUIBuilderContext dynamicUIBuilderContext, List<dynamic> result) {
+    bool divider = child.containsKey("Divider");
+    String key = child["key"];
+    //Будем прихранивать неявно объявленные uuid, что бы небыло лишних перерисовок страницы
+    //Если это повторная отрисовка, данные, которые были на прошлом шагу могли поменятся
+    //Поэтому удалим их и потом заново добавим уже обновлённые
+    removeLastShadowUuid(key, dynamicUIBuilderContext);
+    dynamic value = dynamicUIBuilderContext.dynamicPage.stateData.value[key];
+    bool add = false;
+    if (value != null) {
+      List<dynamic> list = value as List<dynamic>;
+      for (Map<String, dynamic> item in list) {
+        Map<String, dynamic> map = {};
+        map.addAll(child["template"] ??
+            item["template"]); //Шаблон можно заложить в данные
+        map["context"] = item;
+        if (item.containsKey("uuid_data")) {
+          dynamicUIBuilderContext.dynamicPage.addShadowUuid(item["uuid_data"]);
+        }
+        add = true;
+        result.add(map);
+        if (divider && list.last != item) {
+          result.add(child["Divider"]);
+        }
+      }
+    }
+    //Если небыло ничего добавлено в результирующий список, добавим предустановленный ifDataEmpty если есть
+    if (!add && child.containsKey("ifDataEmpty")) {
+      result.add(child["ifDataEmpty"]);
+    }
+  }
+
+  static void removeLastShadowUuid(
+      String key, DynamicUIBuilderContext dynamicUIBuilderContext) {
+    List earlyUuids =
+        dynamicUIBuilderContext.dynamicPage.getProperty(key, []) as List;
+    for (String earlyUuid in earlyUuids) {
+      dynamicUIBuilderContext.dynamicPage.removeShadowUuid(earlyUuid);
+    }
+  }
+
+  static void clickStatic(Map<String, dynamic> parsedJson,
+      DynamicUIBuilderContext dynamicUIBuilderContext,
+      [String key = "onPressed"]) {
+    Map<String, dynamic>? settings =
+        getValueStatic(parsedJson, key, null, dynamicUIBuilderContext);
+    if (settings != null) {
+      if (settings.containsKey('jsInvoke')) {
+        DynamicInvoke().jsInvoke(
+            settings['jsInvoke'], settings['args'], dynamicUIBuilderContext);
+      } else if (settings.containsKey('sysInvoke')) {
+        DynamicInvoke().sysInvoke(settings['sysInvoke'], settings['args'] ?? {},
+            dynamicUIBuilderContext);
+      }
+    }
+  }
+
+  void click(Map<String, dynamic> parsedJson,
+      DynamicUIBuilderContext dynamicUIBuilderContext,
+      [String key = "onPressed"]) {
     clickStatic(parsedJson, dynamicUIBuilderContext, key);
   }
 }
