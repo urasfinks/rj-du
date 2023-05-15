bfb10bf9af9d2ab130f87c5ccac6e152caf631e9
sfinks <urasfinks@yandex.ru>
2023-05-15 20:13:46 +0300

    Загрузка tab из assets

--- lib/db/data_migration.dart
+++ lib/db/data_migration.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:rjdu/dynamic_ui/type_parser.dart';
 import 'package:rjdu/storage.dart';
 import '../data_type.dart';
 import '../global_settings.dart';
@@ -12,7 +13,8 @@ class DataMigration {
   }
 
   migration() async {
-    bool updateApplication = Storage().get('version', 'v0') != GlobalSettings().version;
+    bool updateApplication =
+        Storage().get('version', 'v0') != GlobalSettings().version;
     if (kDebugMode) {
       print("migration() updateApplication = $updateApplication");
     }
@@ -38,8 +40,10 @@ class DataMigration {
       if (kDebugMode) {
         print("Migration: $file");
       }
-      String migration = await rootBundle.loadString('packages/rjdu/lib/assets/$file');
-      List<String> split = migration.split(";"); //sqflite не умеет выполнять скрипт из нескольких запросов (как не странно)
+      String migration =
+          await rootBundle.loadString('packages/rjdu/lib/assets/$file');
+      List<String> split = migration.split(
+          ";"); //sqflite не умеет выполнять скрипт из нескольких запросов (как не странно)
       for (String query in split) {
         query = query.trim();
         if (query.isNotEmpty) {
@@ -49,13 +53,33 @@ class DataMigration {
     }
   }
 
+  static Future<List<String>> loadTabData() async {
+    List<String> result = [];
+    Map assets = json.decode(await rootBundle.loadString('AssetManifest.json'));
+    for (String path in assets.keys) {
+      final regTab = RegExp(r'tab[0-9]+\.json$');
+      if (path.startsWith("assets/db/data/template/") &&
+          regTab.hasMatch(path)) {
+        String fileData = await rootBundle.loadString(path);
+
+        int? index = TypeParser.parseInt(
+            path.split("assets/db/data/template/tab")[1].split(".json")[0]);
+        if (index != null) {
+          result.insert(index, fileData);
+        }
+      }
+    }
+    return result;
+  }
+
   Future<void> loadAssetsData() async {
     Map assets = json.decode(await rootBundle.loadString('AssetManifest.json'));
     for (String path in assets.keys) {
       if (path.startsWith("assets/db/data/")) {
         String fileData = await rootBundle.loadString(path);
         String fileName = path.split("/").last;
-        DataSource().set(fileName, fileData, parseDataTypeFromDirectory(path), null, null, GlobalSettings().debug);
+        DataSource().set(fileName, fileData, parseDataTypeFromDirectory(path),
+            null, null, GlobalSettings().debug);
       }
     }
   }
--- lib/rjdu.dart
+++ lib/rjdu.dart
@@ -1,10 +1,15 @@
 library rjdu;
 
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
+import 'package:rjdu/db/data_migration.dart';
+import 'package:rjdu/dynamic_page.dart';
 import 'package:rjdu/global_settings.dart';
 import 'package:rjdu/system_notify.dart';
 import 'package:rjdu/translate.dart';
 import 'package:uuid/uuid.dart';
+import 'bottom_tab_item.dart';
 import 'data_sync.dart';
 import 'db/data_source.dart';
 import 'dynamic_invoke/dynamic_invoke.dart';
@@ -30,8 +35,24 @@ class RjDu {
     DataSync().init();
 
     SystemNotify().listen(SystemNotifyEnum.changeTabOrHistoryPop, (state) {
-      //print("onRenderOtherPage: $state;");
       NavigatorApp.getLast()?.renderFloatingActionButton();
+    });
+  }
+
+  static Future<DynamicPage> runApp() async {
+    List<String> loadTabData = await DataMigration.loadTabData();
+    for(String tabData in loadTabData){
+      NavigatorApp.tab.add(
+        BottomTabItem(json.decode(tabData)),
+      );
+    }
+    return DynamicPage(const {
+      'flutterType': 'Notify',
+      'link': {'template': 'main.json'},
+      'linkContainer': 'root',
+      'linkDefault': {
+        'template': {'flutterType': 'MaterialApp'}
+      }
     });
   }
 }
