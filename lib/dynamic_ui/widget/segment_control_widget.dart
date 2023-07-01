de44ee7146cd86deb4871ba556b1dede68b46001
sfinks <urasfinks@yandex.ru>
2023-07-01 14:05:59 +0300

    Добавлена обёртка, а то не рендерится

--- lib/dynamic_ui/widget/segment_control_widget.dart
+++ lib/dynamic_ui/widget/segment_control_widget.dart
@@ -20,40 +20,55 @@ class SegmentControlWidget extends AbstractWidget {
     for (Widget w in children) {
       ch[count++] = w;
     }
+    if (value < 0) {
+      value = 0;
+    }
+    if (value > ch.length) {
+      value = ch.length - 1;
+    }
 
-    return CustomSlidingSegmentedControl(
-      key: Util.getKey(),
-      children: ch,
-      decoration: render(parsedJson, 'decoration', null, dynamicUIBuilderContext),
-      thumbDecoration: render(parsedJson, 'thumbDecoration', null, dynamicUIBuilderContext),
-      duration: Duration(
-        milliseconds: TypeParser.parseInt(
-          getValue(parsedJson, 'duration', 300, dynamicUIBuilderContext),
+    bool onChangedNotify = TypeParser.parseBool(
+      getValue(parsedJson, 'onChangedNotify', true, dynamicUIBuilderContext),
+    )!;
+
+    return SizedBox(
+      height: TypeParser.parseDouble(
+        getValue(parsedJson, 'height', 40, dynamicUIBuilderContext),
+      ),
+      child: CustomSlidingSegmentedControl(
+        key: Util.getKey(),
+        children: ch,
+        decoration: render(parsedJson, 'decoration', null, dynamicUIBuilderContext),
+        thumbDecoration: render(parsedJson, 'thumbDecoration', null, dynamicUIBuilderContext),
+        duration: Duration(
+          milliseconds: TypeParser.parseInt(
+            getValue(parsedJson, 'duration', 300, dynamicUIBuilderContext),
+          )!,
+        ),
+        fixedWidth: TypeParser.parseDouble(
+          getValue(parsedJson, 'fixedWidth', null, dynamicUIBuilderContext),
+        ),
+        height: TypeParser.parseDouble(
+          getValue(parsedJson, 'height', 40, dynamicUIBuilderContext),
+        ),
+        padding: TypeParser.parseDouble(
+          getValue(parsedJson, 'padding', 12, dynamicUIBuilderContext),
+        )!,
+        fromMax: TypeParser.parseBool(
+          getValue(parsedJson, 'fromMax', false, dynamicUIBuilderContext),
+        )!,
+        isStretch: TypeParser.parseBool(
+          getValue(parsedJson, 'isStretch', true, dynamicUIBuilderContext),
+        )!,
+        onValueChanged: (int index) {
+          dynamicUIBuilderContext.dynamicPage.setStateData(key, index, onChangedNotify);
+          click(parsedJson, dynamicUIBuilderContext, "onChanged");
+        },
+        initialValue: value,
+        innerPadding: TypeParser.parseEdgeInsets(
+          getValue(parsedJson, 'padding', 2.0, dynamicUIBuilderContext),
         )!,
       ),
-      fixedWidth: TypeParser.parseDouble(
-        getValue(parsedJson, 'fixedWidth', null, dynamicUIBuilderContext),
-      ),
-      height: TypeParser.parseDouble(
-        getValue(parsedJson, 'height', null, dynamicUIBuilderContext),
-      ),
-      padding: TypeParser.parseDouble(
-        getValue(parsedJson, 'padding', 12, dynamicUIBuilderContext),
-      )!,
-      fromMax: TypeParser.parseBool(
-        getValue(parsedJson, 'fromMax', false, dynamicUIBuilderContext),
-      )!,
-      isStretch: TypeParser.parseBool(
-        getValue(parsedJson, 'isStretch', true, dynamicUIBuilderContext),
-      )!,
-      onValueChanged: (int index) {
-        dynamicUIBuilderContext.dynamicPage.setStateData(key, index);
-        click(parsedJson, dynamicUIBuilderContext, "onChanged");
-      },
-      initialValue: value,
-      innerPadding: TypeParser.parseEdgeInsets(
-        getValue(parsedJson, 'padding', 2.0, dynamicUIBuilderContext),
-      )!,
     );
   }
 }
