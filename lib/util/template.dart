d24f070dc7c574aea54f5f03b0f64c7c0a58f7de
sfinks <urasfinks@yandex.ru>
2023-07-01 17:58:54 +0300

    Логирование ошибки шаблонизации

--- lib/util/template.dart
+++ lib/util/template.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:rjdu/util/template/directive.dart';
 import 'package:rjdu/util/template/function.dart';
 
@@ -14,8 +15,7 @@ class Template {
     }
   }
 
-  static String template(
-      String template, DynamicUIBuilderContext dynamicUIBuilderContext,
+  static String template(String template, DynamicUIBuilderContext dynamicUIBuilderContext,
       [bool autoEscape = true, debug = false]) {
     if (!template.contains('\${')) {
       return template;
@@ -32,8 +32,7 @@ class Template {
       }
       String templateName = exp2[0];
       List<String> expDirective = exp2[0].split("|");
-      dynamic value =
-          parseTemplateQuery(expDirective.removeAt(0), dynamicUIBuilderContext);
+      dynamic value = parseTemplateQuery(expDirective.removeAt(0), dynamicUIBuilderContext);
       // if (autoEscape == true && expDirective.isEmpty) {
       //   value = jsonStringEscape(value);
       // }
@@ -41,12 +40,11 @@ class Template {
         for (String directive in expDirective) {
           for (MapEntry<
                   String,
-                  dynamic Function(dynamic data, List<String> arguments,
-                      DynamicUIBuilderContext dynamicUIBuilderContext)> item
+                  dynamic Function(
+                      dynamic data, List<String> arguments, DynamicUIBuilderContext dynamicUIBuilderContext)> item
               in TemplateDirective.map.entries) {
             if (directive.startsWith("${item.key}(")) {
-              List<String> arguments = parseArguments(directive.substring(
-                  item.key.length + 1, directive.length - 1));
+              List<String> arguments = parseArguments(directive.substring(item.key.length + 1, directive.length - 1));
               value = item.value(value, arguments, dynamicUIBuilderContext);
               break;
             }
@@ -58,21 +56,17 @@ class Template {
     return template;
   }
 
-  static dynamic parseTemplateQuery(
-      String query, DynamicUIBuilderContext dynamicUIBuilderContext) {
+  static dynamic parseTemplateQuery(String query, DynamicUIBuilderContext dynamicUIBuilderContext) {
     if (!query.contains("(")) {
       query = "context($query)";
     }
     for (MapEntry<
-            String,
-            dynamic Function(Map<String, dynamic> data, List<String> arguments,
-                DynamicUIBuilderContext dynamicUIBuilderContext)> item
-        in TemplateFunction.map.entries) {
+        String,
+        dynamic Function(Map<String, dynamic> data, List<String> arguments,
+            DynamicUIBuilderContext dynamicUIBuilderContext)> item in TemplateFunction.map.entries) {
       if (query.startsWith("${item.key}(")) {
-        List<String> arguments = parseArguments(
-            query.substring(item.key.length + 1, query.length - 1));
-        return item.value(
-            dynamicUIBuilderContext.data, arguments, dynamicUIBuilderContext);
+        List<String> arguments = parseArguments(query.substring(item.key.length + 1, query.length - 1));
+        return item.value(dynamicUIBuilderContext.data, arguments, dynamicUIBuilderContext);
       }
     }
     return "Undefined handler for: $query";
@@ -86,19 +80,27 @@ class Template {
     return result;
   }
 
-  static dynamic stringSelector(Map<String, dynamic> data, String path,
-      [dynamic defaultValue]) {
+  static dynamic stringSelector(Map<String, dynamic> data, String path, [dynamic defaultValue]) {
     defaultValue ??= "[$path]";
-    List<String> exp = path.split(".");
-    dynamic cur = data;
     bool find = true;
-    for (String key in exp) {
-      if (cur != null && cur[key] != null) {
-        cur = cur[key];
-      } else {
-        find = false;
-        break;
+    dynamic cur = data;
+    try {
+      List<String> exp = path.split(".");
+      for (String key in exp) {
+        if (cur != null && cur[key] != null) {
+          cur = cur[key];
+        } else {
+          find = false;
+          break;
+        }
       }
+    } catch (e, stacktrace) {
+      if (kDebugMode) {
+        print(e);
+        print(stacktrace);
+        print("Exception arg: path: $path; defaultValue: $defaultValue; data: $data");
+      }
+      find = false;
     }
     if (!find) {
       return defaultValue;
@@ -128,8 +130,8 @@ class Template {
     // }
   }
 
-  static Map<String, dynamic> templateArguments(Map<String, dynamic> args,
-      DynamicUIBuilderContext dynamicUIBuilderContext) {
+  static Map<String, dynamic> templateArguments(
+      Map<String, dynamic> args, DynamicUIBuilderContext dynamicUIBuilderContext) {
     if (args.containsKey("templateArguments")) {
       Map<String, dynamic> newArgs = {};
       newArgs.addAll(args);
@@ -141,8 +143,7 @@ class Template {
     return args;
   }
 
-  static void tmp(
-      String path, Map data, DynamicUIBuilderContext dynamicUIBuilderContext) {
+  static void tmp(String path, Map data, DynamicUIBuilderContext dynamicUIBuilderContext) {
     List<String> exp = path.split(".");
     dynamic cur = data;
     dynamic curParent = data;
