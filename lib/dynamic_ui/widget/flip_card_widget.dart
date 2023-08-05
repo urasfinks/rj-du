3a5a73a650d6f2cbd3dc8507f693d1260e17de84
sfinks <urasfinks@yandex.ru>
2023-08-05 22:06:52 +0300

    Доработка API для swipable stack + flip card

--- lib/dynamic_ui/widget/flip_card_widget.dart
+++ lib/dynamic_ui/widget/flip_card_widget.dart
@@ -3,72 +3,85 @@ import 'package:rjdu/dynamic_ui/widget/abstract_widget.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
 
-import '../dynamic_ui.dart';
+import '../../data_type.dart';
+import '../../db/data.dart';
+import '../../db/data_source.dart';
 import '../type_parser.dart';
 
 class FlipCardWidget extends AbstractWidget {
   @override
   get(Map<String, dynamic> parsedJson, DynamicUIBuilderContext dynamicUIBuilderContext) {
-    String key = parsedJson["key"];
-    Map<String, dynamic> defControl = {
-      "value": 0,
-      "isBack": TypeParser.parseBool(
-        getValue(parsedJson, 'isBack', false, dynamicUIBuilderContext),
-      )!,
-      "flip": TypeParser.parseBool(
-        getValue(parsedJson, 'flip', false, dynamicUIBuilderContext),
-      )!
-    };
-    Map<String, dynamic> stateControl = dynamicUIBuilderContext.dynamicPage.getStateData(key, defControl, true);
-    bool isBack = stateControl["isBack"];
-    return GestureDetector(
-      onTap: () {
-        dynamicUIBuilderContext.dynamicPage.dynamicPageSate!.setState(() {
-          stateControl["isBack"] = !stateControl["isBack"];
-          stateControl["flip"] = true;
-        });
+    Map<String, dynamic> stateControl = getStateControl(
+      parsedJson["key"],
+      dynamicUIBuilderContext,
+      {
+        "angle": 0,
+        "isBack": TypeParser.parseBool(
+          getValue(parsedJson, 'isBack', false, dynamicUIBuilderContext),
+        )!,
+        "flip": TypeParser.parseBool(
+          getValue(parsedJson, 'flip', false, dynamicUIBuilderContext),
+        )!
       },
-      child: stateControl["flip"]
-          ? TweenAnimationBuilder(
-              tween: Tween<double>(begin: isBack ? 0 : 180, end: isBack ? 180 : 0),
-              duration: Duration(
-                milliseconds: TypeParser.parseInt(
-                  getValue(parsedJson, 'duration', 350, dynamicUIBuilderContext),
-                )!,
-              ),
-              builder: (context, val, __) {
-                bool isSideBack = val >= (180 / 2);
-                if (isSideBack == isBack) {
-                  stateControl["flip"] = false;
-                }
-                stateControl["value"] = val;
-                Widget widget = render(
+    );
+    Data stateData = Data(parsedJson["key"], stateControl, DataType.virtual, null);
+    DynamicUIBuilderContext newContext = dynamicUIBuilderContext.cloneWithNewData({"state": stateControl});
+    return newContext.dynamicPage.storeValueNotifier.getWidget(
+      {"state": parsedJson["key"]},
+      newContext,
+      (context, child) {
+        bool isBack = stateControl["isBack"];
+        return GestureDetector(
+          onTap: () {
+            stateControl["isBack"] = !stateControl["isBack"];
+            stateControl["flip"] = true;
+            click(parsedJson, newContext, "onTap");
+            DataSource().setData(stateData, true);
+          },
+          child: stateControl["flip"]
+              ? TweenAnimationBuilder(
+                  tween: Tween<double>(begin: isBack ? 0 : 180, end: isBack ? 180 : 0),
+                  duration: Duration(
+                    milliseconds: TypeParser.parseInt(
+                      getValue(parsedJson, 'duration', 350, newContext),
+                    )!,
+                  ),
+                  builder: (context, val, __) {
+                    bool isSideBack = val >= (180 / 2);
+                    if (isSideBack == isBack && stateControl["flip"] == true) {
+                      stateControl["flip"] = false;
+                      click(parsedJson, newContext, isBack ? 'onFlipBack' : 'onFlipFront');
+                    }
+                    stateControl["angle"] = val;
+                    Widget widget = render(
+                      parsedJson,
+                      (isSideBack ? "back" : "front"),
+                      const SizedBox(),
+                      newContext,
+                    );
+                    return Transform(
+                      alignment: Alignment.center,
+                      transform: Matrix4.identity()
+                        ..setEntry(3, 2, 0.001)
+                        ..rotateY(val * pi / 180),
+                      child: isSideBack
+                          ? Transform(
+                              transform: Matrix4.identity()..rotateY(pi),
+                              alignment: Alignment.center,
+                              child: widget,
+                            )
+                          : widget,
+                    );
+                  },
+                )
+              : render(
                   parsedJson,
-                  (isSideBack ? "back" : "front"),
-                  DynamicUI.ui["SizedBox"]!(parsedJson, dynamicUIBuilderContext),
-                  dynamicUIBuilderContext,
-                );
-                return Transform(
-                  alignment: Alignment.center,
-                  transform: Matrix4.identity()
-                    ..setEntry(3, 2, 0.001)
-                    ..rotateY(val * pi / 180),
-                  child: isSideBack
-                      ? Transform(
-                          transform: Matrix4.identity()..rotateY(pi),
-                          alignment: Alignment.center,
-                          child: widget,
-                        )
-                      : widget,
-                );
-              },
-            )
-          : render(
-              parsedJson,
-              (isBack ? "back" : "front"),
-              DynamicUI.ui["SizedBox"]!(parsedJson, dynamicUIBuilderContext),
-              dynamicUIBuilderContext,
-            ),
+                  (isBack ? "back" : "front"),
+                  const SizedBox(),
+                  newContext,
+                ),
+        );
+      },
     );
   }
 }
--- lib/dynamic_ui/widget/swipable_stack_widget.dart
+++ lib/dynamic_ui/widget/swipable_stack_widget.dart
@@ -1,44 +1,153 @@
+import 'dart:async';
 import 'dart:math';
 
-import 'package:flutter/foundation.dart';
 import 'package:rjdu/dynamic_ui/dynamic_ui_builder_context.dart';
 import 'package:swipable_stack/swipable_stack.dart';
 import 'package:flutter/material.dart';
 
 import '../dynamic_ui.dart';
+import '../type_parser.dart';
 import 'abstract_widget.dart';
 
 class SwipableStackWidget extends AbstractWidget {
   @override
   get(Map<String, dynamic> parsedJson, DynamicUIBuilderContext dynamicUIBuilderContext) {
-    return SwipableStack(
-      detectableSwipeDirections: const {
-        SwipeDirection.right,
-        SwipeDirection.left,
+    Map<String, dynamic> stateControl = getStateControl(
+      parsedJson["key"],
+      dynamicUIBuilderContext,
+      {
+        "index": 0,
+        "swipedIndex": null,
+        "swipedDirection": null,
+        "overlayDirection": null,
+        "overlayOpacity": 0,
       },
-      stackClipBehaviour: Clip.none,
+    );
+    //print("SwipableStack key: ${parsedJson["key"]}; state: $stateControl");
+    Set<SwipeDirection> swipeDirection = {};
+    if (parsedJson.containsKey("directions")) {
+      List list = parsedJson["directions"];
+      for (String direction in list) {
+        switch (direction.toString()) {
+          case "right":
+            swipeDirection.add(SwipeDirection.right);
+            break;
+          case "left":
+            swipeDirection.add(SwipeDirection.left);
+            break;
+          case "up":
+            swipeDirection.add(SwipeDirection.up);
+            break;
+          case "down":
+            swipeDirection.add(SwipeDirection.down);
+            break;
+        }
+      }
+    } else {
+      swipeDirection.add(SwipeDirection.right);
+      swipeDirection.add(SwipeDirection.left);
+    }
+
+    dynamic swipeAnchor;
+    if (parsedJson.containsKey("swipeAnchor")) {
+      switch (parsedJson["swipeAnchor"]) {
+        case "top":
+          swipeAnchor = SwipeAnchor.top;
+          break;
+        case "bottom":
+          swipeAnchor = SwipeAnchor.bottom;
+          break;
+      }
+    }
+
+    final controller = SwipableStackController();
+    controller.addListener(() {
+      stateControl["index"] = controller.currentIndex;
+    });
+
+    List children = [];
+    if (parsedJson.containsKey('children')) {
+      children = updateList(parsedJson["children"] as List, dynamicUIBuilderContext);
+    }
+
+    //print("SwipableStackWidget children: $children");
+    Timer? timer;
+    bool roll = TypeParser.parseBool(
+      getValue(parsedJson, 'roll', true, dynamicUIBuilderContext),
+    )!;
+    int rollIndex = TypeParser.parseInt(
+      getValue(parsedJson, 'rollIndex', 0, dynamicUIBuilderContext),
+    )!;
+    return SwipableStack(
+      viewFraction: TypeParser.parseDouble(
+        getValue(parsedJson, 'viewFraction', 0.94, dynamicUIBuilderContext),
+      )!,
+      allowVerticalSwipe: TypeParser.parseBool(
+        getValue(parsedJson, 'allowVerticalSwipe', true, dynamicUIBuilderContext),
+      )!,
+      swipeAnchor: swipeAnchor,
+      controller: controller,
+      detectableSwipeDirections: swipeDirection,
+      stackClipBehaviour: TypeParser.parseClip(
+        getValue(parsedJson, 'stackClipBehaviour', 'none', dynamicUIBuilderContext),
+      )!,
       onSwipeCompleted: (index, direction) {
-        if (kDebugMode) {
-          print('$index, $direction');
+        stateControl["swipedIndex"] = index;
+        stateControl["swipedDirection"] = direction.name.toString();
+        // if (kDebugMode) {
+        //   print('onSwipeCompleted $index, $direction');
+        // }
+        click(parsedJson, dynamicUIBuilderContext, "onSwipeCompleted");
+        if (controller.currentIndex == children.length - 1) {
+          if (roll) {
+            if (timer != null) {
+              timer!.cancel();
+            }
+            int rollDelay = TypeParser.parseInt(
+              getValue(parsedJson, 'rollDelay', 0, dynamicUIBuilderContext),
+            )!;
+            timer = Timer(Duration(seconds: rollDelay), () {
+              controller.currentIndex = rollIndex;
+            });
+          }
+          click(parsedJson, dynamicUIBuilderContext, "onFinish");
         }
       },
-      horizontalSwipeThreshold: 0.7,
-      verticalSwipeThreshold: 0.7,
+      onWillMoveNext: (index, direction) {
+        stateControl["swipedIndex"] = index;
+        stateControl["swipedDirection"] = direction.name.toString();
+        // if (kDebugMode) {
+        //   print('onWillMoveNext $index, $direction');
+        // }
+        click(parsedJson, dynamicUIBuilderContext, "onWillMoveNext");
+        return true;
+      },
+      horizontalSwipeThreshold: TypeParser.parseDouble(
+        getValue(parsedJson, 'horizontalSwipeThreshold', 0.7, dynamicUIBuilderContext),
+      )!,
+      verticalSwipeThreshold: TypeParser.parseDouble(
+        getValue(parsedJson, 'verticalSwipeThreshold', 0.7, dynamicUIBuilderContext),
+      )!,
       overlayBuilder: (context, properties) {
-        final opacity = min(properties.swipeProgress, 1.0);
-        String to = properties.direction == SwipeDirection.right ? "Знаю" : "Не знаю";
-        return Opacity(
-          opacity: opacity,
-          child: Text(
-            to,
-            textAlign: TextAlign.end,
-          ),
+        stateControl["overlayOpacity"] = min(properties.swipeProgress, 1.0);
+        stateControl["overlayDirection"] = properties.direction.name.toString();
+        return render(
+          parsedJson,
+          "overlay",
+          DynamicUI.ui["SizedBox"]!(parsedJson, dynamicUIBuilderContext),
+          dynamicUIBuilderContext,
         );
       },
       builder: (context, properties) {
-        //final itemIndex = properties.index;
-        return render(parsedJson, "children", DynamicUI.ui["SizedBox"]!(parsedJson, dynamicUIBuilderContext),
-            dynamicUIBuilderContext);
+        if (properties.index < children.length) {
+          return render(children[properties.index], null, const SizedBox(), dynamicUIBuilderContext);
+        } else {
+          if (roll) {
+            return render(children[rollIndex], null, const SizedBox(), dynamicUIBuilderContext);
+          } else {
+            return const SizedBox();
+          }
+        }
       },
     );
   }
