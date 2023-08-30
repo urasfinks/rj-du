98939a7b484dbd8734cdf5ecd5fe0f5af45f5db0
sfinks <urasfinks@yandex.ru>
2023-08-30 22:30:33 +0300

    Исправление багов после перехода на множественные состояния

--- lib/dynamic_invoke/handler/set_state_data_handler.dart
+++ lib/dynamic_invoke/handler/set_state_data_handler.dart
@@ -7,8 +7,10 @@ class SetStateDataHandler extends AbstractHandler {
   @override
   handle(Map<String, dynamic> args, DynamicUIBuilderContext dynamicUIBuilderContext) {
     if (Util.containsKeys(args, ["key", "value"])) {
-      dynamicUIBuilderContext.dynamicPage.stateData.set(args["state"], args["key"], args["value"], args["notify"] ?? true);
+      dynamicUIBuilderContext.dynamicPage.stateData
+          .set(args["state"], args["key"], args["value"], args["notify"] ?? true);
     } else if (Util.containsKeys(args, ["map"])) {
+      //Util.log("SetStateDataHandler args: $args");
       dynamicUIBuilderContext.dynamicPage.stateData.setMap(args["state"], args["map"], args["notify"] ?? true);
     } else {
       if (kDebugMode) {
