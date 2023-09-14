9a2a0c4c2c0ae785f68f871ed9572a664b156694
sfinks <urasfinks@yandex.ru>
2023-09-14 15:31:39 +0300

    Почти убран bar separator)

--- lib/global_settings.dart
+++ lib/global_settings.dart
@@ -18,7 +18,7 @@ class GlobalSettings {
   double bottomNavigationBarHeight = 56.0;
   String orientation = ""; //landscape/portrait
   int debugStackTraceMaxFrames = 10;
-  double barSeparatorOpacity = 0.05;
+  double barSeparatorOpacity = 0.01;
 
   static final GlobalSettings _singleton = GlobalSettings._internal();
 
