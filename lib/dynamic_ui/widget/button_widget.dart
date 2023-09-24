13b1cce566a037785811be3fce08cb3c7a93e18f
sfinks <urasfinks@yandex.ru>
2023-09-24 10:51:06 +0300

    Управление только высотой кнопки

--- lib/dynamic_ui/widget/button_widget.dart
+++ lib/dynamic_ui/widget/button_widget.dart
@@ -1,4 +1,6 @@
+import '../../util.dart';
 import '../dynamic_ui_builder_context.dart';
+import '../type_parser.dart';
 import '../widget/abstract_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -9,27 +11,21 @@ import 'button/text_button_style.dart';
 
 class ButtonWidget extends AbstractWidget {
   @override
-  get(Map<String, dynamic> parsedJson,
-      DynamicUIBuilderContext dynamicUIBuilderContext) {
+  get(Map<String, dynamic> parsedJson, DynamicUIBuilderContext dynamicUIBuilderContext) {
     AbstractButtonStyle? styleResource;
-    String? buttonStyle = getValue(
-        parsedJson, "buttonStyle", "Elevated", dynamicUIBuilderContext);
+    String? buttonStyle = getValue(parsedJson, "buttonStyle", "Elevated", dynamicUIBuilderContext);
     switch (buttonStyle) {
       case "Elevated":
-        styleResource =
-            ElevatedButtonStyle(dynamicUIBuilderContext, this, parsedJson);
+        styleResource = ElevatedButtonStyle(dynamicUIBuilderContext, this, parsedJson);
         break;
       case "Outlined":
-        styleResource =
-            OutlinedButtonStyle(dynamicUIBuilderContext, this, parsedJson);
+        styleResource = OutlinedButtonStyle(dynamicUIBuilderContext, this, parsedJson);
         break;
       case "Text":
-        styleResource =
-            TextButtonStyle(dynamicUIBuilderContext, this, parsedJson);
+        styleResource = TextButtonStyle(dynamicUIBuilderContext, this, parsedJson);
         break;
     }
-    String? borderStyle = getValue(parsedJson, "borderStyle",
-        "RoundedRectangleBorder", dynamicUIBuilderContext);
+    String? borderStyle = getValue(parsedJson, "borderStyle", "RoundedRectangleBorder", dynamicUIBuilderContext);
     ButtonStyle? resultButtonStyle;
     if (borderStyle != null && styleResource != null) {
       switch (borderStyle) {
@@ -44,6 +40,19 @@ class ButtonWidget extends AbstractWidget {
           break;
       }
     }
-    return styleResource!.getButton(resultButtonStyle);
+    if (parsedJson.containsKey("height") || parsedJson.containsKey("width")) {
+      return SizedBox(
+        key: Util.getKey(),
+        width: TypeParser.parseDouble(
+          getValue(parsedJson, "width", null, dynamicUIBuilderContext),
+        ),
+        height: TypeParser.parseDouble(
+          getValue(parsedJson, "height", null, dynamicUIBuilderContext),
+        ),
+        child: styleResource!.getButton(resultButtonStyle),
+      );
+    } else {
+      return styleResource!.getButton(resultButtonStyle);
+    }
   }
 }
