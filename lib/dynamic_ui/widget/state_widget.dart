47465fbf26293fe4a2345690919a06fb55434222
sfinks <urasfinks@yandex.ru>
2023-10-18 09:57:55 +0300

    Если default не будет заполнен, отрисуем заглушку в виде текста

--- lib/dynamic_ui/widget/state_widget.dart
+++ lib/dynamic_ui/widget/state_widget.dart
@@ -2,12 +2,16 @@ import 'package:rjdu/dynamic_ui/dynamic_ui_builder_context.dart';
 
 import '../dynamic_ui.dart';
 import 'abstract_widget.dart';
+import 'package:flutter/material.dart';
 
 class StateWidget extends AbstractWidget {
   @override
   get(Map<String, dynamic> parsedJson, DynamicUIBuilderContext dynamicUIBuilderContext) {
-    Map<String, dynamic> data =
-        dynamicUIBuilderContext.dynamicPage.stateData.get(parsedJson["state"], parsedJson["key"], {});
+    Map<String, dynamic>? data = dynamicUIBuilderContext.dynamicPage.stateData
+        .get(parsedJson["state"], parsedJson["key"], parsedJson["default"]);
+    if (data == null) {
+      return Text("StateWidget($parsedJson) is null");
+    }
     return render(
       data,
       null,
