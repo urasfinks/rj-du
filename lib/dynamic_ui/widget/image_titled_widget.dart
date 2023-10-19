a27ba35c2e2b0763d34bd762cf35f98ecb489156
sfinks <urasfinks@yandex.ru>
2023-10-19 20:02:49 +0300

    Добавлена конфигурация ImageTitledWidget

--- lib/dynamic_ui/widget/image_titled_widget.dart
+++ lib/dynamic_ui/widget/image_titled_widget.dart
@@ -2,46 +2,50 @@ import 'package:rjdu/dynamic_ui/dynamic_ui_builder_context.dart';
 import 'package:rjdu/dynamic_ui/widget/abstract_widget.dart';
 import 'package:flutter/material.dart';
 
+import '../type_parser.dart';
+
 class ImageTitledWidget extends AbstractWidget {
   @override
   get(Map<String, dynamic> parsedJson, DynamicUIBuilderContext dynamicUIBuilderContext) {
-    double margin = 5.0;
-    double radius = 15.0;
-    String label = "Hello";
-    String src = "https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80";
-    return Container(
-      margin: EdgeInsets.all(margin),
-      child: ClipRRect(
-          borderRadius: BorderRadius.all(Radius.circular(radius)),
-          child: Stack(
-            children: <Widget>[
-              Image.network(src, fit: BoxFit.cover, width: 1000.0),
-              Positioned(
-                bottom: 0.0,
-                left: 0.0,
-                right: 0.0,
-                child: Container(
-                  decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [Color.fromARGB(200, 0, 0, 0), Color.fromARGB(0, 0, 0, 0)],
-                      begin: Alignment.bottomCenter,
-                      end: Alignment.topCenter,
-                    ),
+    double radius = TypeParser.parseDouble(
+      getValue(parsedJson, "radius", 8.0, dynamicUIBuilderContext),
+    )!;
+    String label = getValue(parsedJson, "label", "", dynamicUIBuilderContext).toString();
+    double fontSize = TypeParser.parseDouble(
+      getValue(parsedJson, "fontSize", 15, dynamicUIBuilderContext),
+    )!;
+
+    return ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(radius)),
+        child: Stack(
+          children: <Widget>[
+            render(parsedJson, "image", const SizedBox(), dynamicUIBuilderContext),
+            Positioned(
+              bottom: 0.0,
+              left: 0.0,
+              right: 0.0,
+              child: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Color.fromARGB(200, 0, 0, 0), Color.fromARGB(0, 0, 0, 0)],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
                   ),
-                  padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                  child: Text(
-                    label,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                    ),
+                ),
+                padding: TypeParser.parseEdgeInsets(
+                  getValue(parsedJson, "padding", "10,0,10,10", dynamicUIBuilderContext),
+                )!,
+                child: Text(
+                  label,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: fontSize,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
-            ],
-          )),
-    );
+            ),
+          ],
+        ));
   }
-
 }
