01299247efc23683087af2bd525ca2efd99fe095
sfinks <urasfinks@yandex.ru>
2023-10-20 17:51:21 +0300

    Добавлен Positioned.fill

--- lib/dynamic_ui/widget/positioned_widget.dart
+++ lib/dynamic_ui/widget/positioned_widget.dart
@@ -7,27 +7,47 @@ import 'package:flutter/material.dart';
 class PositionedWidget extends AbstractWidget {
   @override
   get(Map<String, dynamic> parsedJson, DynamicUIBuilderContext dynamicUIBuilderContext) {
-    return Positioned(
-      key: Util.getKey(),
-      height: TypeParser.parseDouble(
-        getValue(parsedJson, "height", null, dynamicUIBuilderContext),
-      ),
-      width: TypeParser.parseDouble(
-        getValue(parsedJson, "width", null, dynamicUIBuilderContext),
-      ),
-      left: TypeParser.parseDouble(
-        getValue(parsedJson, "left", null, dynamicUIBuilderContext),
-      ),
-      right: TypeParser.parseDouble(
-        getValue(parsedJson, "right", null, dynamicUIBuilderContext),
-      ),
-      top: TypeParser.parseDouble(
-        getValue(parsedJson, "top", null, dynamicUIBuilderContext),
-      ),
-      bottom: TypeParser.parseDouble(
-        getValue(parsedJson, "bottom", null, dynamicUIBuilderContext),
-      ),
-      child: render(parsedJson, "child", null, dynamicUIBuilderContext),
-    );
+    switch (parsedJson["type"] ?? "default") {
+      case "fill":
+        return Positioned.fill(
+          key: Util.getKey(),
+          left: TypeParser.parseDouble(
+            getValue(parsedJson, "left", null, dynamicUIBuilderContext),
+          ),
+          right: TypeParser.parseDouble(
+            getValue(parsedJson, "right", null, dynamicUIBuilderContext),
+          ),
+          top: TypeParser.parseDouble(
+            getValue(parsedJson, "top", null, dynamicUIBuilderContext),
+          ),
+          bottom: TypeParser.parseDouble(
+            getValue(parsedJson, "bottom", null, dynamicUIBuilderContext),
+          ),
+          child: render(parsedJson, "child", null, dynamicUIBuilderContext),
+        );
+      default:
+        return Positioned(
+          key: Util.getKey(),
+          height: TypeParser.parseDouble(
+            getValue(parsedJson, "height", null, dynamicUIBuilderContext),
+          ),
+          width: TypeParser.parseDouble(
+            getValue(parsedJson, "width", null, dynamicUIBuilderContext),
+          ),
+          left: TypeParser.parseDouble(
+            getValue(parsedJson, "left", null, dynamicUIBuilderContext),
+          ),
+          right: TypeParser.parseDouble(
+            getValue(parsedJson, "right", null, dynamicUIBuilderContext),
+          ),
+          top: TypeParser.parseDouble(
+            getValue(parsedJson, "top", null, dynamicUIBuilderContext),
+          ),
+          bottom: TypeParser.parseDouble(
+            getValue(parsedJson, "bottom", null, dynamicUIBuilderContext),
+          ),
+          child: render(parsedJson, "child", null, dynamicUIBuilderContext),
+        );
+    }
   }
 }
