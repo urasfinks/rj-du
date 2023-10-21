b10c86248638f7e3965b6ec527b77e4bb904ea13
sfinks <urasfinks@yandex.ru>
2023-10-21 10:26:24 +0300

    Переработан ImageTitledWidget с добавлением в стек новых элементов и onTap

--- lib/dynamic_ui/widget/image_titled_widget.dart
+++ lib/dynamic_ui/widget/image_titled_widget.dart
@@ -14,38 +14,65 @@ class ImageTitledWidget extends AbstractWidget {
     double fontSize = TypeParser.parseDouble(
       getValue(parsedJson, "fontSize", 15, dynamicUIBuilderContext),
     )!;
-
+    dynamic extraElement = const SizedBox();
+    // Условия получились конечно не очень привычные и не очень красивые
+    // Я испытывал проблемы с динамическим шаблоном, когда extra всегда заполнено из контекста
+    // Можно лишь управлять содержимым extra вот так и пришлось выкручиваться
+    parsedJson["extra"] = getValue(parsedJson, "extra", "", dynamicUIBuilderContext);
+    if (parsedJson.containsKey("extra") && parsedJson["extra"].toString() != "") {
+      extraElement = render(parsedJson, parsedJson["extra"], const SizedBox(), dynamicUIBuilderContext);
+    }
+    parsedJson["onTap"] = getValue(parsedJson, "onTap", "", dynamicUIBuilderContext);
+    if (parsedJson.containsKey("onTap") && parsedJson["onTap"].toString() != "") {
+      extraElement = Positioned.fill(
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: () {
+              click(parsedJson, dynamicUIBuilderContext, "onTap");
+            },
+            child: extraElement,
+          ),
+        ),
+      );
+    } else {
+      extraElement = Positioned.fill(
+        child: extraElement,
+      );
+    }
     return ClipRRect(
-        borderRadius: BorderRadius.all(Radius.circular(radius)),
-        child: Stack(
-          children: <Widget>[
-            render(parsedJson, "image", const SizedBox(), dynamicUIBuilderContext),
-            Positioned(
-              bottom: 0.0,
-              left: 0.0,
-              right: 0.0,
-              child: Container(
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [Color.fromARGB(200, 0, 0, 0), Color.fromARGB(0, 0, 0, 0)],
-                    begin: Alignment.bottomCenter,
-                    end: Alignment.topCenter,
-                  ),
+      borderRadius: BorderRadius.all(Radius.circular(radius)),
+      child: Stack(
+        children: <Widget>[
+          render(parsedJson, "image", const SizedBox(), dynamicUIBuilderContext),
+          Positioned(
+            bottom: 0.0,
+            left: 0.0,
+            right: 0.0,
+            child: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Color.fromARGB(200, 0, 0, 0), Color.fromARGB(0, 0, 0, 0)],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
                 ),
-                padding: TypeParser.parseEdgeInsets(
-                  getValue(parsedJson, "padding", "10,0,10,5", dynamicUIBuilderContext),
-                )!,
-                child: Text(
-                  label,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: fontSize,
-                    fontWeight: FontWeight.bold,
-                  ),
+              ),
+              padding: TypeParser.parseEdgeInsets(
+                getValue(parsedJson, "padding", "10,0,10,5", dynamicUIBuilderContext),
+              )!,
+              child: Text(
+                label,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: fontSize,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-          ],
-        ));
+          ),
+          extraElement
+        ],
+      ),
+    );
   }
 }
