25ccebc82cfc261ab6603a19ea8ea509329b0d6d
sfinks <urasfinks@yandex.ru>
2023-10-23 01:59:29 +0300

    Добавлен Scaffold backgroundColor для возможности выставить transparent для dialog

--- lib/dynamic_ui/widget/scaffold_widget.dart
+++ lib/dynamic_ui/widget/scaffold_widget.dart
@@ -1,13 +1,19 @@
 import '../dynamic_ui_builder_context.dart';
 import '../type_parser.dart';
 import '../widget/abstract_widget.dart';
-import '../../util.dart';
 import 'package:flutter/material.dart';
 
 class ScaffoldWidget extends AbstractWidget {
   @override
   Widget get(Map<String, dynamic> parsedJson, DynamicUIBuilderContext dynamicUIBuilderContext) {
+    // Если использовать привычный Util.getKey() - то получаем ошибку:
+    // At this point the state of the widget's element tree is no longer stable
+    // При попытке вызова метода HideHandler(snackBar)
+    final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
     return Scaffold(
+      backgroundColor: TypeParser.parseColor(
+        getValue(parsedJson, "backgroundColor", null, dynamicUIBuilderContext),
+      ),
       extendBodyBehindAppBar: TypeParser.parseBool(
         getValue(parsedJson, "extendBodyBehindAppBar", true, dynamicUIBuilderContext),
       )!,
@@ -16,7 +22,7 @@ class ScaffoldWidget extends AbstractWidget {
       )!,
       appBar: render(parsedJson, "appBar", null, dynamicUIBuilderContext),
       body: render(parsedJson, "body", const SizedBox(), dynamicUIBuilderContext),
-      key: Util.getKey(),
+      key: _scaffoldKey,
     );
   }
 }
